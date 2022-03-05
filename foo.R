0975d7fd45d9336e50c3fa176f8904216b463e7e
Lee Hin Sing Raymond <u3509094@connect.hku.hk>
2022-03-06 01:18:01 +0800

    Changed the constant 3/4 to 4/3 in the volume function

--- foo.R
+++ foo.R
@@ -3,7 +3,7 @@ r <- 2
 
 # Function to compute the volume of a sphere with radius r
 volume <- function(r) {
-  3/4*pi*r^2
+  4/3*pi*r^2
 }
 
 # Function to compute the volumes of the spheres with radius r, r^2 and r^3
